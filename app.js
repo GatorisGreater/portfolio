10c4682a365ee300afca6ce17a4d84f4542921e5
gatorisgreater <william.broadway@gmail.com>
2017-02-07 09:32:27 -0500

    started state object and state management framework

---
+++ app.js
@@ -0,0 +1,14 @@
+const SEARCH_URL = 'http://quotes.stormconsultancy.co.uk/random.json';
+
+
+const state = {
+	projects: [{
+		projectTitle: "Quiz App",
+		projectDescription: "User plays a Simpsons trivia game. The user gets a chance to answer 5 questions and receive right or wrong feedback on each selection. The user cannot skip a question without answering. After the 5th question the user receives their final score and the option to start the quiz over."
+		projectStack: [HTML, CSS, JavaScript, jQuery, Basic State Management Principles],
+		projectScreenShot: "",
+		projectCodeLink: "",
+		projectDemoLink: "",
+		projectImprovements: "Refactoring to ES6 syntax"
+	}]
+}
