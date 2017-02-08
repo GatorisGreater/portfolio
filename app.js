becbfe2f057d3288e13ddbef78d13e43fbec493d
gatorisgreater <william.broadway@gmail.com>
2017-02-08 09:49:54 -0500

    implemented AJAX logic and updated html wireframe to include space for quote generator

--- app.js
+++ app.js
@@ -1,14 +1,60 @@
-const SEARCH_URL = 'http://quotes.stormconsultancy.co.uk/random.json';
+const SEARCH_URL = 'https://cors.now.sh/http://quotes.stormconsultancy.co.uk/random.json';
+// const SEARCH_URL = 'http://quotes.stormconsultancy.co.uk/random.json';
 
 
+// Initial State Object
 const state = {
 	projects: [{
 		projectTitle: "Quiz App",
-		projectDescription: "User plays a Simpsons trivia game. The user gets a chance to answer 5 questions and receive right or wrong feedback on each selection. The user cannot skip a question without answering. After the 5th question the user receives their final score and the option to start the quiz over."
-		projectStack: [HTML, CSS, JavaScript, jQuery, Basic State Management Principles],
+		projectDescription: "User plays a Simpsons trivia game. The user gets a chance to answer 5 questions and receive right or wrong feedback on each selection. The user cannot skip a question without answering. After the 5th question the user receives their final score and the option to start the quiz over.",
+		projectStack: ["HTML", "CSS", "JavaScript", "jQuery", "Basic State Management Principles"],
 		projectScreenShot: "",
 		projectCodeLink: "",
 		projectDemoLink: "",
 		projectImprovements: "Refactoring to ES6 syntax"
-	}]
+	},
+	{
+		projectTitle: "Weather Challenge",
+		projectDescription: "User can see which of 2 cities has better weather. User inputs city#1 in the left-hand input box and submits to get basic current weather information about that city. User inputs city#2 in the right-hand input box to get basic current weather information about that city. The user then selects the `{Who Wins?}` button to run a calculation to determine which city has the better weather and the prevailing city is displayed. If the result is a tie, the user is given a feedback message saying it's a tie.",
+		projectStack: ["HTML", "CSS", "Mobile Viewing`/Responsive Grid`", "JavaScript", "jQuery", "RESTful API communication", "Basic State Management Principles"],
+		projectScreenShot: "",
+		projectCodeLink: "",
+		projectDemoLink: "",
+		projectImprovements: "Refactoring to ES6 syntax"
+	},
+	{
+		projectTitle: "Repo Tracker",
+		projectDescription: "User can track projects and details. This information is stored in a database for future recall.",
+		projectStack: ["MongoDB", "Express", "`Node.js`", "`React/Redux`", "HTML", "CSS", "JavaScript", "JSX"],
+		projectScreenShot: "",
+		projectCodeLink: "",
+		projectDemoLink: "",
+		projectImprovements: "Refactoring to ES6 syntax"
+	},
+	]
 }
+
+//State Manipulation Functions
+
+//Render Functions
+// function renderProject(state, index) {
+// 	let stackArray = state.projects[index].projectStack.map(tool => {
+// 		return <li> {tool} </li>
+// 	});
+// 	$('.body-projectdetails p').html(state.projects[index].projectTitle);
+// 	$('.body-projectdetails ul').html(stackArray);
+// }
+
+//AJAX Logic
+
+const quoteGenerator = (callback) => $.getJSON(SEARCH_URL, callback);
+
+function callback(response) {
+	console.log(Object.keys(response));
+	console.log(response.quote);
+	console.log(response.author);
+}
+
+quoteGenerator(callback);
+
+//Event Handlers
